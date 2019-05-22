216606a5d53161033b2b4444d4f18c4f65bfe713
Felipe Fernandez <heyitsfelipe@gmail.com>
2019-05-21 18:37:39 -0700

    Allows a show to be fetched upon click

--- pages/tvshows.js
+++ pages/tvshows.js
@@ -1,32 +1,59 @@
 import Layout from '../components/Layout';
 import fetch from 'isomorphic-unfetch';
+import { Component } from 'react';
 
-const Tvshows = (props) => (
-    <Layout>
-        <div className="grid-container leader-1">
-            <div className="column-24">
-                <h1>TV Shows</h1>
-            </div>
-            <ul>
-                {props.shows.map(show => (
-                    <li key={show.id}>{show.name}</li>
-                )
-                )}
-            </ul>
-        </div>
-    </Layout>
-);
+class Tvshows extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            shows: []
+        };
+    }
 
-Tvshows.getInitialProps = async function () {
-    const res = await fetch('https://api.tvmaze.com/search/shows?q=rick%20and%20morty')
-    const data = await res.json()
+    handleClick = async (event) => {
+        console.log('Clicked');
+        const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+        const data = await res.json()
 
-    console.log(`Show data fetched. Count: ${data.length}`)
-    console.log(data);
+        this.setState({
+            shows: data.map(entry => entry.show)
+        });
+    }
 
-    return {
-        shows: data.map(entry => entry.show)
+    render() {
+        let shows = [];
+        if (this.state.shows.length > 0) {
+            shows = this.state.shows.map(show => (
+                <li key={show.id}>{show.name}</li>
+            ));
+        }
+        return (
+            <Layout>
+                <div className="grid-container leader-1">
+                    <div className="column-24">
+                        <h1>TV Shows</h1>
+                    </div>
+                    <button onClick={this.handleClick}>Fetch</button>
+                    <ul>
+                        {shows}
+                    </ul>
+                </div>
+            </Layout>
+        );
     }
 }
+
+// --- This code may be useful if we want an initial fetch from the api ---
+// Tvshows.getInitialProps = async function () {
+//     const res = await fetch('https://api.tvmaze.com/search/shows?q=rick%20and%20morty')
+//     const data = await res.json()
+
+//     console.log(`Show data fetched. Count: ${data.length}`)
+//     console.log(data);
+
+//     return {
+//         shows: data.map(entry => entry.show)
+//     }
+// }
 
 export default Tvshows;
