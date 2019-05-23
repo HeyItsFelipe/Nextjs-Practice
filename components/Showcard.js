62b38a8a73e01c8db6989ad26028f355ed77f992
Felipe Fernandez <heyitsfelipe@gmail.com>
2019-05-23 10:34:08 -0700

    Handles missing images from api

--- components/Showcard.js
+++ components/Showcard.js
@@ -1,8 +1,9 @@
 const showCard = ({ show }) => (
     <div key={show.id} className="card block trailer-1">
-        <figure className="card-image-wrap">
-            <img className="card-image" src={show.image.medium} alt={show.name} />
-        </figure>
+        {show.image ?
+            <figure className="card-image-wrap">
+                <img className="card-image" src={show.image.medium} alt={show.name} />
+            </figure> : ''}
         <div className="card-content">
             <h4><a href={show.url} target="_blank">{show.name}</a></h4>
             <p className="font-size--1 card-last" dangerouslySetInnerHTML={{ __html: show.summary }}></p>
