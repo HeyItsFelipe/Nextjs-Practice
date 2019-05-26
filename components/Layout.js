c008c79eaa5eebc70d40b17b71999e4a2938bc34
Felipe Fernandez <heyitsfelipe@gmail.com>
2019-05-25 22:12:46 -0700

    Adds server and favicon

--- components/Layout.js
+++ components/Layout.js
@@ -4,6 +4,7 @@ const Layout = (props) => (
     <div>
         <Head>
             <title>Nextjs Practice</title>
+            <link rel="icon" type="image/png" sizes="16x16" href="../static/favicon-16x16.png" />
             <link rel="stylesheet" href="https://s3-us-west-1.amazonaws.com/patterns.esri.com/files/calcite-web/1.2.5/css/calcite-web.min.css" />
         </Head>
         {props.children}
