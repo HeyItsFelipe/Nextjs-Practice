7a6bfd4ec28c999236ec1f554fe470d19cd5ce50
Felipe Fernandez <heyitsfelipe@gmail.com>
2019-05-26 19:13:39 -0700

    Updates tvshowfinder with react hooks

--- pages/tvshowfinder.js
+++ pages/tvshowfinder.js
@@ -1,60 +1,51 @@
 import Layout from '../components/Layout';
 import fetch from 'isomorphic-unfetch';
-import { Component } from 'react';
 import Showgallery from '../components/Showgallery';
+import { useState } from 'react';
 
-class Tvshows extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            shows: [],
-            inputValue: ''
-        };
-    }
+function Tvshows() {
 
-    getTvShows = async () => {
-        const res = await fetch('https://api.tvmaze.com/search/shows?q=' + this.state.inputValue)
+    const [shows, setShows] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+
+    const getTvShows = async () => {
+        const res = await fetch('https://api.tvmaze.com/search/shows?q=' + inputValue)
         const data = await res.json()
         console.log(data);
-        this.setState({
-            shows: data.map(entry => entry.show)
-        });
+        setShows(data.map(entry => entry.show));
     }
 
-    handleInputChange = (event) => {
-        this.setState({
-            inputValue: event.target.value
-        });
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
     }
 
     // Tv show data is fetched from api when enter key is pressed.
-    handleInputKeyPress = (event) => {
+    const handleInputKeyPress = (event) => {
         if (event.keyCode === 13) {
-            this.getTvShows();
+            getTvShows();
         }
     }
 
-    render() {
-        return (
-            <Layout>
-                <div className="grid-container leader-1">
-                    <div className="column-24">
-                        <h1>TV Show Finder</h1>
-                    </div>
-                    <div className="column-8 center-column">
-                        <div className="input-group">
-                            <input className="input-group-input" type="text" placeholder="Enter TV Show"
-                                onChange={this.handleInputChange} onKeyDown={this.handleInputKeyPress} />
-                            <span className="input-group-button">
-                                <button className="btn" onClick={this.getTvShows}>Submit</button>
-                            </span>
-                        </div>
-                    </div>
-                    <Showgallery shows={this.state.shows} />
+    return (
+        <Layout>
+            <div className="grid-container leader-1">
+                <div className="column-24">
+                    <h1>TV Show Finder</h1>
                 </div>
-            </Layout>
-        );
-    }
+                <div className="column-8 center-column">
+                    <div className="input-group">
+                        <input className="input-group-input" type="text" placeholder="Enter TV Show"
+                            onChange={handleInputChange} onKeyDown={handleInputKeyPress} />
+                        <span className="input-group-button">
+                            <button className="btn" onClick={getTvShows}>Submit</button>
+                        </span>
+                    </div>
+                </div>
+                <Showgallery shows={shows} />
+            </div>
+        </Layout>
+    );
+
 }
 
 
