2664ddcf8009f7eb89eb68d25d0a3c141a42144f
Felipe Fernandez <heyitsfelipe@gmail.com>
2019-05-27 09:24:14 -0700

    Initial commit for todolist

---
+++ pages/todolist.js
@@ -0,0 +1,49 @@
+import Layout from '../components/Layout';
+import { useState } from 'react';
+
+function Todolist() {
+
+    const [list, setList] = useState([
+        {
+            value: 'Study',
+            isCompleted: false
+        },
+        {
+            value: 'Eat',
+            isCompleted: false
+        },
+        {
+            value: 'Sleep',
+            isCompleted: false
+        }
+
+    ]);
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    }
+
+    return (
+        <Layout>
+            <div className="grid-container leader-1">
+                <div className="column-24">
+                    <h1>To Do List</h1>
+                </div>
+                <div className="column-8 post-16">
+                    <div className="input-group">
+                        <input className="input-group-input" type="text" placeholder="Enter To Do Item"
+                            onChange={handleInputChange} />
+                        <span className="input-group-button">
+                            <button className="btn">Submit</button>
+                        </span>
+                    </div>
+                </div>
+                {list.map((listItem, index) => <div key={index}>{listItem.value}</div>)}
+            </div>
+        </Layout>
+    );
+
+}
+
+export default Todolist;
