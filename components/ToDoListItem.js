4d48ae3d870fc94b565b4467e79632dffb8f0bf0
Felipe Fernandez <heyitsfelipe@gmail.com>
2019-06-02 21:05:16 -0700

    Moves onClick from icons to buttons

--- components/ToDoListItem.js
+++ components/ToDoListItem.js
@@ -5,11 +5,11 @@ const ToDoListItem = ({ listItem, index, deleteListItem, strikeListItem }) => (
                 {listItem.title}
             </span>
         </div>
-        <button className="btn-red btn-grouped">
-            <span className="font-size-3 icon-ui-close icon-ui-red" aria-label="delete" onClick={() => deleteListItem(index)}></span>
+        <button className="btn-red btn-grouped" onClick={() => deleteListItem(index)}>
+            <span className="font-size-3 icon-ui-close icon-ui-red" aria-label="delete"></span>
         </button>
-        <button className="btn-green btn-grouped">
-            <span className="font-size-3 icon-ui-check-mark icon-ui-gray" aria-label="done" onClick={() => strikeListItem(index)}></span>
+        <button className="btn-green btn-grouped" onClick={() => strikeListItem(index)}>
+            <span className="font-size-3 icon-ui-check-mark icon-ui-gray" aria-label="done"></span>
         </button>
     </div>
 );
